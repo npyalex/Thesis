4d925841fa448de91e53b8d9d114fe5a56ec6ea2
npyalex <43121843+npyalex@users.noreply.github.com>
2020-02-03 11:51:36 -0500

    2020-02-03
    Actor Manager written

--- Assets/00_Project Files/Scripts/ActorManager.cs
+++ Assets/00_Project Files/Scripts/ActorManager.cs
@@ -1,93 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
 
 public class ActorManager : MonoBehaviour
 {
-    //private int advance;
-    //public GameObject PaintTrigger, HideTrigger, CastleTrigger;
-    //public FadeInTimer userBlindfold, titleText;
-    public ViveControllerManager controller;
+    private int advance;
+    public GameObject PaintTrigger, HideTrigger, CastleTrigger;
+    public FadeInTimer userBlindfold, titleText;
     //timer variables
-    //private float timer;
-    //public float timerMax;
-    //private bool hasRun = false;
+    private float timer;
+    public float countdownTimerMax;
+    private bool hasRun = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        //advance = 1;
+        advance = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (timer >= timerMax && hasRun == false)
-        //{
-        //    advance++;
-        //    hasRun = true;
-        //} else {
-        //    hasRun = false;
+        Debug.Log("Advance = " + advance);
+        //Debug.Log("HasRun = " + hasRun);
+
+        if ((Input.GetButton("Fire1") || Input.GetButton("Fire2")) && hasRun == false)
+        {
+            //Debug.Log("Grab Held");
+            timer += Time.deltaTime;
+            if (timer >= countdownTimerMax )
+            {
+                //Debug.Log("Trigger Activated");
+                advance++;
+                hasRun = true;
+            }
+        }
+        if (Input.GetButtonUp("Fire1") || Input.GetButtonUp("Fire2"))
+        {
+            timer = 0.0f;
+            hasRun = false;
+        }
+
+        if (advance == 1)
+        {
+            //Game Start State
+            PaintTrigger.SetActive(false);
+            HideTrigger.SetActive(false);
+            CastleTrigger.SetActive(false);
+        }
+        else if (advance == 2)
+        {
+            //Debug.Log("Fade Stage");
+            //Fade out blindfold
+            userBlindfold.Fade();
+        }
+        //else if (advance == 3)
+        //    {
+        //        //Hub State
+        //        StartHub();
+        //    } else if (advance == 4)
+        //    {
+        //        //Game state
+        //    } else if (advance == 5)
+        //    {
+        //        //outro state
+        //    } else if (advance == 6)
+        //    {
+        //        //end game state
+        //    }
         //}
 
-        if (controller.GetGrab())
-        {
-            Debug.Log("Grab Held");
-            //    timer += Time.deltaTime;
-            //}
-            //else
-            //{
-            //    timer = 0.0f;
-            //}
-        }
+        //public void StartHub()
+        //{
+        //    PaintTrigger.SetActive(true);
+        //    HideTrigger.SetActive(true);
+        //    CastleTrigger.SetActive(true);
+        //}
+
+        //public void StartCastle()
+        //{
+
+        //}
+
+        //public void StartHideNSeek()
+        //{
+
+        //}
+
+        //public void StartPainting()
+        //{
+
+        //}
     }
-
-    //    if (advance == 1)
-    //    {
-    //        //Game Start State
-    //        PaintTrigger.SetActive(false);
-    //        HideTrigger.SetActive(false);
-    //        CastleTrigger.SetActive(false);
-    //    }
-    //    else if (advance == 2)
-    //    {
-    //        //Fade out blindfold
-    //        userBlindfold.Fade();
-    //    } else if (advance == 3)
-    //    {
-    //        //Hub State
-    //        StartHub();
-    //    } else if (advance == 4)
-    //    {
-    //        //Game state
-    //    } else if (advance == 5)
-    //    {
-    //        //outro state
-    //    } else if (advance == 6)
-    //    {
-    //        //end game state
-    //    }
-    //}
-
-    //public void StartHub()
-    //{
-    //    PaintTrigger.SetActive(true);
-    //    HideTrigger.SetActive(true);
-    //    CastleTrigger.SetActive(true);
-    //}
-
-    //public void StartCastle()
-    //{
-
-    //}
-
-    //public void StartHideNSeek()
-    //{
-
-    //}
-
-    //public void StartPainting()
-    //{
-
-    //}
 }
