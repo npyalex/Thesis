a4f5cc6aea7e27e16b8a321d903497c962617d5b
npyalex <43121843+npyalex@users.noreply.github.com>
2020-04-09 13:47:29 -0400

    2020-04-09
    After defense

---
+++ Assets/00_Project Files/Scripts/CountdownAndScreenshot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownAndScreenshot : MonoBehaviour
+{
+    public Screenshotter screenshotter;
+    public AudioSource audioSource;
+    public int countdownTime;
+    public AudioClip tick, camClick;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeScreenshot()
+    {
+        StartCoroutine("Countdown");
+    }
+
+    IEnumerator Countdown()
+    {
+        audioSource.PlayOneShot(tick, 1.0f);
+        yield return new WaitForSeconds(countdownTime);
+        screenshotter.TakeScreenshot();
+        audioSource.PlayOneShot(camClick, 1.0f);
+    }
+}
---
+++ Assets/00_Project Files/Scripts/CountdownTimerUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimerUI : MonoBehaviour
+{
+    public float timeLeft = 3.0f;
+    public bool stop = true;
+
+    private float seconds;
+
+    private Text text;
+
+    public void startTimer(float from)
+    {
+        stop = false;
+        timeLeft = from;
+        GameObject go;
+        GameObject myText;
+        go = new GameObject();
+        go.name = "TempUI";
+        go.AddComponent<Canvas>();
+        Canvas myCanvas = go.GetComponent<Canvas>();
+        myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        myText = new GameObject();
+        myText.transform.parent = go.transform;
+        text = myText.AddComponent<Text>();
+        Update();
+        StartCoroutine(updateCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stop) return;
+        timeLeft -= Time.deltaTime;
+        seconds = timeLeft % 60;
+    }
+
+    private IEnumerator updateCoroutine()
+    {
+        while(!stop)
+        {
+            text.text = string.Format("{0:0}:{1:00}", seconds);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
